f5e3f3ece42745528e19c80bba9197963416d608
comunitius <54519239+comunitius@users.noreply.github.com>
2019-09-02 21:17:44 -0700

    Feat circle (#1)
    * chore - add react applicatiopn
    
    * chore - add nest
    
    * setup docker
    
    * add circleci

---
+++ apps/app/src/app/app.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { App } from './app';
+//hello
+
+describe('App', () => {
+  afterEach(cleanup);
+
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    expect(getByText('Welcome to app!')).toBeTruthy();
+  });
+});
